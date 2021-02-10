b2daea892a63a344be53b5b44bc5f39c995be203
Bianca <biancancanal@gmail.com>
2021-02-10 17:09:42 -0600

    commit tree ba260722e5bd4e5decb4925ca7b7136664bc85ee

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,28 @@
 def using_include(array, element)
-
+    array.include?(element)
 end
 
 def using_sort(array)
-
+    names = ["I", "arrays", "wow"]
+    names.sort  
 end
 
 def using_reverse(array)
-
+    names = ["wow","I","arrays!"]
+    names.reverse  
 end
 
 def using_first(array)
-
+    names = ["wow","I","arrays!"]
+    names.first  
 end
 
 def using_last(array)
-
+    names = ["wow","I","arrays!"]
+    names.last  
 end
 
 def using_size(array)
-
+    names = ["wow","I","arrays!","why","lab","picky"]
+    names.size  
 end
